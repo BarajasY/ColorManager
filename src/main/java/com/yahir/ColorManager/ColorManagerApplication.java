8366916349a032a00088cfc286fd222a00da4606
Yahir Moreno Barajas <100240827+Kanomb@users.noreply.github.com>
2023-02-21 16:57:57 -0600

    Added create method

--- src/main/java/com/yahir/ColorManager/ColorManagerApplication.java
+++ src/main/java/com/yahir/ColorManager/ColorManagerApplication.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yahir.ColorManager.Color.Color;
@@ -12,6 +16,7 @@ import com.yahir.ColorManager.Color.ColorRepository;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/colors")
 public class ColorManagerApplication {
 
 	private final ColorRepository colorRepository;
@@ -24,9 +29,25 @@ public class ColorManagerApplication {
 		SpringApplication.run(ColorManagerApplication.class, args);
 	}
 
-	@GetMapping("/")
+	@CrossOrigin
+	@GetMapping
 	public List<Color> getColors() {
 		return colorRepository.findAll();
+	}
+
+	record NewColorRequest(
+		String color1,
+		String color2,
+		String color3
+	) {}
+
+	@PostMapping
+	public void createColors(@RequestBody NewColorRequest request) {
+		Color Color = new Color();
+		Color.setColor1(request.color1());
+		Color.setColor2(request.color2());
+		Color.setColor3(request.color3());
+		colorRepository.save(Color);
 	}
 
 }
